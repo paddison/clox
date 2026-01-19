38c3b3785351575509b7d178e89034368e6b7e60
paddison <patrickbaumann579@gmail.com>
2026-01-19 11:51:21 +0000

    finish chapter 30

--- value.c
+++ value.c
@@ -38,6 +38,18 @@ void freeValueArray(ValueArray *array) {
 }
 
 void printValue(Value value) {
+// Note: I would need to adapt this for the other values as well
+#ifdef NAN_BOXING
+  if (IS_BOOL(value)) {
+    printf(AS_BOOL(value) ? "true" : "false");
+  } else if (IS_NIL(value)) {
+    printf("nil");
+  } else if (IS_NUMBER(value)) {
+    printf("%g", AS_NUMBER(value));
+  } else if (IS_OBJ(value)) {
+    printObject(value);
+  }
+#else
   switch (value.type) {
   case VAL_BOOL:
     printf(AS_BOOL(value) ? "true" : "false");
@@ -58,9 +70,16 @@ void printValue(Value value) {
     printf("%s", AS_ERR(value));
     break;
   }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+  if (IS_NUMBER(a) && IS_NUMBER(b)) {
+    return AS_NUMBER(a) && AS_NUMBER(b);
+  }
+  return a == b;
+#else
   if (a.type != b.type)
     return false;
   switch (a.type) {
@@ -75,4 +94,5 @@ bool valuesEqual(Value a, Value b) {
   default:
     return false;
   }
+#endif
 }
